4c47a694068c8756848ba8f73ad7b0866be23441
joanaferreira0011 <joanaferreira0011@gmail.com>
2020-05-10 02:42:35 +0100

    added tfidf

--- read_data_set.py
+++ read_data_set.py
@@ -2,6 +2,9 @@ import pandas as pd
 # import stanza
 import nltk
 from nltk.corpus import treebank
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 def read_data(file):
     data = pd.read_csv(file, sep="\t", header=None, skiprows=1)
@@ -12,15 +15,15 @@ def read_data(file):
 
     for index, row in data.iterrows():
         texts[int(row['level'][0])].append(row['text'])
+        # texts[int(row['level'][0])]+=' ' +row['text'])
     
     return texts
 
 
+# tweets= read_data('2018-EI-oc-En-sadness-dev.txt')
+
 tweets= read_data('data_test.txt')
-# stanza.download('en') 
-# nlp = stanza.Pipeline('en')
-# doc = nlp(tweets[0])
-# doc.sentences[0].print_dependencies()
+print(tweets[2])
 
 def get_bag_of_words(tweets):
     bag_of_words= tweets.copy()
@@ -30,8 +33,22 @@ def get_bag_of_words(tweets):
     return bag_of_words
 
 
+# print(get_bag_of_words(tweets))
 
-print(get_bag_of_words(tweets))
+ 
+ 
+# settings that you use for count vectorizer will go here
+tfidf_vectorizer=TfidfVectorizer(use_idf=True)
+ 
+# just send in all your docs here
+tfidf_vectorizer_vectors=tfidf_vectorizer.fit_transform(tweets[2])
 
+# get the first vector out (for the first document)
+first_vector_tfidfvectorizer=tfidf_vectorizer_vectors[1]
 
+df = pd.DataFrame(first_vector_tfidfvectorizer.T.todense(), index=tfidf_vectorizer.get_feature_names(), columns=["tfidf"])
+df=df.sort_values(["tfidf"], ascending=False)
+
+print(df)
+# print(df.at['🙈', "tfidf"])
 
